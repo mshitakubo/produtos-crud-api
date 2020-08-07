17517fc6a505fc571bad96191678755562b1bec7
Marcelo Shitakubo <nostressmeister@gmail.com>
2020-08-07 18:46:23 -0300

    segunda parte da aplicação, funções update e delete, implementadas

--- app.js
+++ app.js
@@ -48,6 +48,23 @@ app.get('/visualizar/:id?',function(req, res){
     }
 })
 
+app.get('/deletar/:id', function(req, res){
+    sql.query("delete from produtos where id=?",[req.params.id])
+    res.render('deletar')
+})
+
+app.get('/atualizar/:id',function(req,res){
+    sql.query("select * from produtos where id=?", [req.params.id], function(err, results, fields){
+        res.render('update',{id:req.params.id, nome:results[0].nome, descricao:results[0].descricao, marca:results[0].marca, 
+            modelo: results[0].modelo, preco:results[0].preco})
+    })
+})
+
+app.post('/controllerUpdate', urlencodeParser, function(req,res){
+    sql.query("update produtos set nome=?, descricao=?, marca=?, modelo=?, preco=? where id=?",
+    [req.body.nome, req.body.descricao, req.body.marca, req.body.modelo, req.body.preco, req.body.id])
+    res.render('controllerUpdate')
+})
 
 
 
